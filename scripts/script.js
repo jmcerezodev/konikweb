200f4c9a3734cae088823d55c177489d8181fd82
JM Cerezo <jmcerezodev@gmail.com>
2024-01-23 16:43:56 +0100

    Copiando arreglos de la web de KINOK 23/01/2024

--- scripts/script.js
+++ scripts/script.js
@@ -8,18 +8,107 @@ $(document).ready(function(){
     let estadoBtnMovil = false;
     let bordeBarraNav = $(".menu-nav li");
     let anchoPantalla = $(window).width();
+
+
+    // Al iniciar web
+
+    if(anchoPantalla < 1200){
+
+        btnMenuMovil.on("click", function () {
+
+                
+            clickBtnMenu();
+            console.log(anchoPantalla);
+            
+        });
+        
+
+        $(".menu-nav li a").on("click", function(){
+
+            cerrarMenuClickEnlace();
+        });
+    }
+
     
 
 
-    //Comprobando ancho de la pantalla
-
+    /*Comprobando si cambia el ancho de la pantalla, 
+    comportamiento al cambiar de ancho una vez iniciada la web*/
+    
     $(window).resize(function () { 
+
         anchoPantalla = $(window).width();
 
-        //Mostrando Navegacion dependiendo el tamaño de la pantalla
+        mostrarTipoMenu();
+        estadoBtnMovil = false;
+
+    });
+
+    
+   
+    //Posicion Scroll barra siempre visible
+    let flechaAbajo = $(".flecha-abajo");
+   
+    $(window).scroll(function () { 
+      
+
+         if (scrollY >= 0.1){
+
+            
+            flechaAbajo.css("display", "none");
+           
+
+         }else{
+           
+             
+             flechaAbajo.css("display", "block");
+           
+         }
+    });
+
+
+
+
+    // CopyRight
+    let textoFooter = $("#TextoFooter");
+    let date = new Date;
+    let ano = date.getFullYear();
+    textoFooter.html("KINOK® " + ano);  
+
+
+
+    ////////////////////////////// FUNCIONES  /////////////////////////////////////////
+
+    function clickBtnMenu(){
+
+        console.log("Click Btn Menu");
+
+         //Click en el boton del menu movil
+         if(estadoBtnMovil == false){
+
+            menuMovil.show("fast");
+            bordeBarraNav.css("border", "none");
+            estadoBtnMovil = true;
+            console.log(estadoBtnMovil);
+
+        }else{
+
+            menuMovil.hide("fast");
+            estadoBtnMovil = false;
+            console.log(estadoBtnMovil);
+        }
+
+           
+
+    }
+
+    //Mostar tipo menu dependiendo del ancho de la pantalla
+    function mostrarTipoMenu(){
+
         if(anchoPantalla > "1200"){
             //Mostar menu el PC
             menuMovil.css("display", "flex");
+            estadoBtnMovil = true;
 
         }else{
 
@@ -27,68 +116,25 @@ $(document).ready(function(){
             menuMovil.css("display", "none");
         }
 
+    }
 
-        //Cerrar menu al tocar
+
+    //Cerrar menu al tocar enlace
+    function cerrarMenuClickEnlace(){
+
         if(anchoPantalla < 1200){
 
-            console.log("Ancho menos a 999px ")
-            $(".menu-nav li a").on("click", function(){
-
-                menuMovil.hide("fast");
-            });
-        }else{
-            menuMovil.show();
-        }
-    });
-
-    
-
-    // Detectando click en el boton del menu movil
-    btnMenuMovil.on("click", function () {
+        menuMovil.hide("fast");
+         estadoBtnMovil = false;
+        //console.log(estadoBtnMovil);
+        console.log("Click! enclace cerrar menu");
         
-
-        if(estadoBtnMovil == false){
-            //menuMovil.css("display", "flex");
-            menuMovil.show("fast");
-            bordeBarraNav.css("border", "none");
-            estadoBtnMovil = true;
-        }else{
-            //menuMovil.css("display", "none");
-            menuMovil.hide("fast");
-            estadoBtnMovil = false;
         }
-    });
+        
+     }
 
-    
-
-    // CopyRight
-    let textoFooter = $("#TextoFooter");
-    let date = new Date;
-    let ano = date.getFullYear();
-    textoFooter.html("KONIK® " + ano);  
-    
-    //Posicion Scroll
-    let flechaAbajo = $(".flecha-abajo");
-    let barraNav = $(".barra-nav");
-    
-    $(window).scroll(function () { 
-        console.log(scrollY)
-
-
-         if (scrollY >= 0.1){
-
-            barraNav.css("width","100%");
-            barraNav.css("position" , "fixed");
-            flechaAbajo.css("display", "none");
-            
-
-         }else{
-            
-             barraNav.css("position", "relative")
-             flechaAbajo.css("display", "block");
-            
-         }
-    });
 
  });
+
+
 
